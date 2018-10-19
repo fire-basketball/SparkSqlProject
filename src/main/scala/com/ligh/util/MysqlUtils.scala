038846259f067826ce2b9d7ad8fe3e55681947a7
ligh <liguohui@huluwa.cc>
2018-10-19 17:35:09 +0800

    ligh

--- src/main/scala/com/ligh/util/MysqlUtils.scala
+++ src/main/scala/com/ligh/util/MysqlUtils.scala
@@ -1,6 +1,8 @@
 package com.ligh.util
 
-import java.sql.DriverManager
+import java.sql.{DriverManager, PreparedStatement}
+
+import com.mysql.jdbc.Connection
 
 /**
   *  Mysql操作工具类
@@ -17,6 +19,31 @@ object MysqlUtils {
     DriverManager.getConnection("jdbc:mysql://47.100.176.47:3306/imooc_project?user=root&password=123456")
   }
 
+  /**
+    *  数据库关闭之后,释放资源
+    * @param connoection
+    * @param pstms
+    */
+  def release(connoection:Connection,pstms:PreparedStatement): Unit ={
+    try{
+      if(pstms != null){
+        pstms.close()
+      }
+    }catch {
+      case e :Exception => e.printStackTrace()
+    }finally {
+      if(connoection != null){
+        connoection.close()
+      }
+    }
+
+
+  }
+
+  /**
+    *  测试连接是否成功
+    * @param args
+    */
   def main(args: Array[String]) {
 
     println(getConnection())
